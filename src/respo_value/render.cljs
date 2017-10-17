179b759d724643b72276919748f1c0e858c458fd
jiyinyiyong <jiyinyiyong@gmail.com>
2017-10-18 00:31:00 +0800

    upgrade deps; update CDN

--- src/respo_value/render.cljs
+++ src/respo_value/render.cljs
@@ -5,7 +5,7 @@
             [respo-value.comp.container :refer [comp-container]]
             [respo-value.schema :as schema]))
 
-(def base-info {:title "Respo Value", :icon "http://logo.respo.site/respo.png", :ssr nil})
+(def base-info {:title "Respo Value", :icon "http://cdn.tiye.me/logo/respo.png", :ssr nil})
 
 (defn dev-page []
   (make-page
@@ -18,15 +18,19 @@
 (defn prod-page []
   (let [html-content (make-string (comp-container schema/store))
         webpack-info (.parse js/JSON (slurp "dist/webpack-manifest.json"))
-        cljs-info (.parse js/JSON (slurp "dist/cljs-manifest.json"))]
+        cljs-info (.parse js/JSON (slurp "dist/cljs-manifest.json"))
+        cdn (if preview? "" "http://cdn.tiye.me/respo-value/")
+        prefix-cdn (fn [x] (str cdn x))]
     (make-page
      html-content
      (merge
       base-info
-      {:styles ["http://repo-cdn.b0.upaiyun.com/favored-fonts/main.css"
-                (aget webpack-info "main.css")],
-       :scripts [(-> cljs-info (aget 0) (aget "js-name"))
-                 (-> cljs-info (aget 1) (aget "js-name"))]}))))
+      {:styles ["http://cdn.tiye.me/favored-fonts/main.css"
+                (prefix-cdn (aget webpack-info "main.css"))],
+       :scripts (map
+                 prefix-cdn
+                 [(-> cljs-info (aget 0) (aget "js-name"))
+                  (-> cljs-info (aget 1) (aget "js-name"))])}))))
 
 (defn main! []
   (if (= js/process.env.env "dev")
